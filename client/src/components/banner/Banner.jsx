02ae34eafa60bb48221394c5cfa3a76828922578
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-07-12 17:52:30 +0300

    Update banner, remove group image, gradient on heading

--- client/src/components/banner/Banner.jsx
+++ client/src/components/banner/Banner.jsx
@@ -7,13 +7,13 @@ export default function Banner() {
         <>
             <div className="banner-wrapper">
                 <div id="index_banner" className="banner-vertical-center-index container container-fluid pt-5 pb-5">
-                    <div className='pb-5'>
+                    {/* <div className='pb-5'>
                         <img className='banner-img' src={banner} alt="" />
-                    </div>
+                    </div> */}
 
                     <div className='group-workouts shadow-lg'>
                         <h1>Find the power of group workouts</h1>
-                        <img className='banner-group-img' src={group} alt="" />
+                        <img className='banner-group-img' src={banner} alt="" />
                     </div>
 
                 </div>
