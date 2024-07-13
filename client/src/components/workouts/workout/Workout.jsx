6364ce06b4aeb5fbc2d5b8385de67f0921d91ffd
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-07-13 21:48:56 +0300

    Add different images for every Workout

--- client/src/components/workouts/workout/Workout.jsx
+++ client/src/components/workouts/workout/Workout.jsx
@@ -1,4 +1,6 @@
-import baby from './baby-power2.jpg';
+import babys from './moms-with-babys2.jpg';
+import functional from './mobility2.jpg';
+import mobility from './pregnant.jpg'
 import './Workout.css';
 import close from './close.png'
 
@@ -7,10 +9,20 @@ export default function Workout({
     onClick,
     onChange,
 }) {
+    let imgSrc = '';
+
+    if (workout.img === 'assets/pregnant.jpg') {
+        imgSrc = mobility;
+    } else if (workout.img === 'assets/mobility2.jpg') {
+        imgSrc = functional;
+    } else if (workout.img === 'assets/moms-with-babys2.jpg') {
+        imgSrc = babys;
+    }
+
     return (
         <div className="col-md-4 mb-3">
             <a className="recent-work card border-0 shadow-lg overflow-hidden" onClick={() => {onClick(), onChange()}}>
-                <img className="recent-work-img card-img" src={baby} alt="Card image" />
+                <img id="workout-img" className="recent-work-img card-img" src={imgSrc} alt="Card image" />
                 <div className="recent-work-vertical card-img-overlay d-flex align-items-end">
                     <div className="recent-work-content text-start mb-3 ml-3 text-dark">
                         <h3 className="card-title light-300">{workout.name}</h3>
