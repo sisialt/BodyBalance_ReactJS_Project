02e7de4fcaa7116ee414d1964a3420103b76da6e
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-07-14 23:00:21 +0300

    Update Banner, add motivation img, update Banner.css

--- client/src/components/banner/Banner.jsx
+++ client/src/components/banner/Banner.jsx
@@ -1,4 +1,5 @@
 import banner from './logo-plus-text.png'
+import oneday from './motivation3.jpg'
 import group from './mobility.jpg'
 import './Banner.css'
 
@@ -7,13 +8,12 @@ export default function Banner() {
         <>
             <div className="banner-wrapper">
                 <div id="index_banner" className="banner-vertical-center-index container container-fluid pt-5 pb-5">
-                    {/* <div className='pb-5'>
-                        <img className='banner-img' src={banner} alt="" />
-                    </div> */}
-
                     <div className='group-workouts shadow-lg'>
-                        <h1>Find the power of group workouts</h1>
-                        <img className='banner-group-img' src={banner} alt="" />
+                        <div className='flex'>
+                            <h1>Discover the Power of Group Workouts with</h1>
+                            <img className='banner-group-img' src={banner} alt="" />
+                        </div>
+                        <img className='banner-group-img2' src={oneday} alt="" />
                     </div>
 
                 </div>
