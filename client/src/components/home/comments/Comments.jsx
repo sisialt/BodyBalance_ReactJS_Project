80134edbf7a29bee567f5649ae7582f53c086b8e
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-07-21 13:23:51 +0300

    Update Comments with data from comments.json

--- client/src/components/home/comments/Comments.jsx
+++ client/src/components/home/comments/Comments.jsx
@@ -1,7 +1,20 @@
-import baby from './body-balance-changerooms.jpg';
+import { useState, useEffect } from 'react';
 import './Comments.css/'
 
 export default function Comments() {
+
+    const baseUrl = 'http://localhost:3030/jsonstore';
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(`${baseUrl}/comments`);
+            const data = await response.json();
+            const commentsData = Object.values(data);
+            setComments(commentsData);
+        })();
+    }, []);
+
     return (
         <>
             <section className="py-5 mb-5">
@@ -11,42 +24,19 @@ export default function Comments() {
                     </div>
                     <div className="row gy-5 g-lg-5 mb-4">
 
-                        <div className="col-md-4 mb-3">
-                            <div className="card shadow-lg overflow-hidden">
-
-                                <div className="  d-flex align-items-center">
-                                    <div className="recent-work-content text-start mb-3 ml-3 text-dark no-decoration">
-                                        <h3 className="card-title light-300">Ivan Ivanov</h3>
-                                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, exercitationem quos quae beatae libero dolorem sapiente tempora consequuntur perspiciatis at non debitis, cumque corrupti quibusdam voluptas ullam iure molestiae soluta!</p>
+                        {comments.map(comment =>
+                            <div key={comment._id} className="col-md-4 mb-3">
+                                <div className="card shadow-lg overflow-hidden">
+                                    <div className="  d-flex align-items-center">
+                                        <div className="recent-work-content text-start mb-3 ml-3 text-dark no-decoration">
+                                            <h3 className="card-title light-300">{comment.username} for {comment.workout}</h3>
+                                            <p>{comment.createdAt}</p>
+                                            <p className="card-text">{comment.text}</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-
-                        <div className="col-md-4 mb-3">
-                            <div className="card shadow-lg overflow-hidden">
-
-                                <div className="  d-flex align-items-center">
-                                    <div className="recent-work-content text-start mb-3 ml-3 text-dark no-decoration">
-                                        <h3 className="card-title light-300">Ivan Ivanov</h3>
-                                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, exercitationem quos quae beatae libero dolorem sapiente tempora consequuntur perspiciatis at non debitis, cumque corrupti quibusdam voluptas ullam iure molestiae soluta!</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-md-4 mb-3">
-                            <div className="card shadow-lg overflow-hidden">
-
-                                <div className="  d-flex align-items-center">
-                                    <div className="recent-work-content text-start mb-3 ml-3 text-dark no-decoration">
-                                        <h3 className="card-title light-300">Ivan Ivanov</h3>
-                                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, exercitationem quos quae beatae libero dolorem sapiente tempora consequuntur perspiciatis at non debitis, cumque corrupti quibusdam voluptas ullam iure molestiae soluta!</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
+                        )}
 
                     </div>
                 </div>
