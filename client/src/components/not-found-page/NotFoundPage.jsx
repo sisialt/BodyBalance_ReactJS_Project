62987f2199111ce380beef94f491ab35d925ff66
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-07-24 22:32:18 +0300

    Add NotFoundPage

--- client/src/components/not-found-page/NotFoundPage.jsx
+++ client/src/components/not-found-page/NotFoundPage.jsx
@@ -3,7 +3,10 @@ import styles from './NotFoundPage.module.css'
 export default function NotFoundPage() {
     return (
         <>
-            <h1 className={styles.notFound}>Page Not Found</h1>
+            <div className={styles['not-found']}>
+                <h2 className="text-center mb-4 h2 semi-bold-600 py-5"><i className={`bx-fw bx bx-meh-alt bx-xs ${styles['sad']}`}></i><br />404 <br />Page not found</h2>
+                <p className={styles.text}>The Page you are looking for doesn't exist.</p>
+            </div>
         </>
     );
 }
