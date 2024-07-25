f66d0b0883ad2a6f2dd655d5c127357bd4b1cb60
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-07-25 22:42:25 +0300

    Fit images, navigate to Not Found Page when not existing workoutId

--- client/src/components/workouts-page/workout-info/WorkoutInfo.jsx
+++ client/src/components/workouts-page/workout-info/WorkoutInfo.jsx
@@ -1,30 +1,37 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const baseUrl = 'http://localhost:3030/jsonstore';
 
 export default function WorkoutInfo() {
     const [workout, setWorkout] = useState({});
     const { workoutId } = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
         (async () => {
-            const response = await fetch(`${baseUrl}/workouts/${workoutId}`);
-            const data = await response.json();
-            const workoutData = data;
-            setWorkout(workoutData);
+            try {
+                const response = await fetch(`${baseUrl}/workouts/${workoutId}`);
+                const data = await response.json();
+                const workoutData = data;
+                setWorkout(workoutData);
+            } catch (error) {
+                navigate('/not-found');
+                return;
+            }
+
         })();
     }, []);
 
     return (
         <>
-            <div style={{paddingTop: 200}}>
+            <div style={{ paddingTop: 200 }}>
                 <h1>{workout.name}</h1>
                 <p>{workout.info}</p>
                 <p>{workout.description}</p>
-                {/* TODO img path <div className="gallery">
+                <div className="gallery">
                     <img src={workout.img} />
-                </div> */}
+                </div>
             </div>
         </>
     );
