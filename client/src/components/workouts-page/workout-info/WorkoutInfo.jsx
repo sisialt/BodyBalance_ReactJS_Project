8bfe89e4abea32e3966388bcb2ed183da9e5a802
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-08-05 22:10:22 +0300

    Implement useGetOneWorkouts in WorkoutInfo

--- client/src/components/workouts-page/workout-info/WorkoutInfo.jsx
+++ client/src/components/workouts-page/workout-info/WorkoutInfo.jsx
@@ -1,36 +1,18 @@
-import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-
-const baseUrl = 'http://localhost:3030/jsonstore';
+import { useParams } from 'react-router-dom';
+import { useGetOneWorkouts } from '../../../hooks/useWorkouts';
 
 export default function WorkoutInfo() {
-    const [workout, setWorkout] = useState({});
     const { workoutId } = useParams();
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        (async () => {
-            try {
-                const response = await fetch(`${baseUrl}/workouts/${workoutId}`);
-                const data = await response.json();
-                const workoutData = data;
-                setWorkout(workoutData);
-            } catch (error) {
-                navigate('/not-found');
-                return;
-            }
-
-        })();
-    }, []);
+    const [workout] = useGetOneWorkouts(workoutId);
 
     return (
         <>
-            <div style={{ paddingTop: 200 }}>
+            <div style={{ paddingTop: 50, marginLeft: 400, marginRight: 400, marginBottom: 50 }}>
                 <h1>{workout.name}</h1>
                 <p>{workout.info}</p>
                 <p>{workout.description}</p>
-                <div className="gallery">
-                    <img src={workout.img} />
+                <div className="gallery" >
+                    <img src={workout.img} style={{ maxWidth: 200 }} />
                 </div>
             </div>
         </>
