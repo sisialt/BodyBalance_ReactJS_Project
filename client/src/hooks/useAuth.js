cc8083379ea79a6b59a8a901b626f1df7bc7e548
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-08-05 15:37:36 +0300

    Create useAuth with useLogin, useRegister functions

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,31 @@
+import { login, register } from "../api/auth-api.js";
+import { AuthContext } from "../contexts/AuthContext.js";
+import { useContext } from "react";
+
+export const useLogin = () => {
+    const { changeAuthState } = useContext(AuthContext);
+
+    const loginHandler = async (email, password) => {
+        const { password: _, ...authData } = await login(email, password);
+
+        changeAuthState(authData);
+
+        return authData;
+    }
+
+    return loginHandler;
+};
+
+export const useRegister = () => {
+    const { changeAuthState } = useContext(AuthContext);
+
+    const registerHandler = async (email, password) => {
+        const { password: _, ...authData } = await register(email, password);
+
+        changeAuthState(authData);
+
+        return authData;
+    }
+
+    return registerHandler;
+};
