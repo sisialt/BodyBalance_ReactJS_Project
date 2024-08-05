cd7394d44d5f779034095b78077b825ec35bb83d
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-08-05 22:11:07 +0300

    Implement useGetOneTrainers in TrainerInfo

--- client/src/components/our-team-page/trainer-info/TrainerInfo.jsx
+++ client/src/components/our-team-page/trainer-info/TrainerInfo.jsx
@@ -1,36 +1,19 @@
-import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
-const baseUrl = 'http://localhost:3030/jsonstore';
+import { useGetOneTrainers } from '../../../hooks/useTrainers';
 
 export default function TrainerInfo() {
-    const [trainer, setTrainer] = useState({});
     const { trainerId } = useParams();
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        (async () => {
-            try {
-                const response = await fetch(`${baseUrl}/trainers/${trainerId}`);
-                const data = await response.json();
-                const trainerData = data;
-                setTrainer(trainerData);
-            } catch (error) {
-                navigate('/not-found');
-                return;
-            }
-
-        })();
-    }, []);
+    const [trainer] = useGetOneTrainers(trainerId);
 
     return (
         <>
-            <div style={{ paddingTop: 200 }}>
+            <div style={{ paddingTop: 50, marginLeft: 400, marginRight: 400, marginBottom: 50 }}>
                 <h1>{trainer.firstName} {trainer.lastName}</h1>
                 <p>{trainer.info}</p>
                 <p>Workouts: {trainer.workouts}</p>
-                <div className="gallery">
-                    <img src={trainer.img} />
+                <div className="gallery" >
+                    <img src={trainer.img} style={{ maxWidth: 200 }} />
                 </div>
             </div>
         </>
