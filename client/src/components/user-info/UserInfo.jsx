6fbf8850b67841c0ff600235950cf2fa36faa3a9
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-08-06 15:53:10 +0300

    Create UserInfo component with logout link

---
+++ client/src/components/user-info/UserInfo.jsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { Link } from "react-router-dom";
+
+export default function UserInfo() {
+    const contextData = useContext(AuthContext);
+
+    return (
+        <>
+            <div className="container user-info-page" style={{ paddingTop: 50 }}>
+                <h1 style={{textAlign: "center"}}>User Info</h1>
+
+                <form style={{maxWidth: 500, marginBottom: 50}}>
+
+                    <div className="user-info-container" style={{display: "flex", flexDirection: "column", gap: 20, maxWidth: 500}}>
+                        <p>Email: {contextData.email}</p>
+                        <p>Password: ******</p>
+
+                        <Link to="/logout"><input type="submit" className="submit" value="Logout" /></Link>
+
+                    </div>
+
+                </form>
+            </div>
+        </>
+    );
+}
