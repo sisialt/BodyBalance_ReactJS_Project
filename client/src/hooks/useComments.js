4387ced31c751b21cc383fedf9c6bca614a4966f
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-08-07 19:57:29 +0300

    Create useComments with useGetAllComments, useGetOneWorkoutComments, useCreate, useGetLatest, useDeleteComment

---
+++ client/src/hooks/useComments.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+
+import commentsAPI from "../api/comments-api";
+import { useNavigate } from "react-router-dom";
+
+export function useGetAllComments() {
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const result = await commentsAPI.getAll();
+
+            setComments(result);
+        })();
+    }, []);
+
+    return [comments, setComments];
+}
+
+export function useGetOneWorkoutComments(workoutId) {
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const result = await commentsAPI.getAllForOneWorkout(workoutId);
+            
+            setComments(result);
+        })();
+    }, [workoutId]);
+
+    return [comments, setComments];
+}
+
+export function useCreateComment() {
+    const commentCreateHandler = (workoutId, text) => commentsAPI.create(workoutId, text);
+
+    return commentCreateHandler;
+}
+
+export function useGetLatestThreeComments() {
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const result = await commentsAPI.getLatestThree();
+
+            setComments(result);
+        })();
+    }, []);
+
+    return [comments, setComments];
+}
+
+export function useDeleteComment() {
+    const commentDeleteHandler = (commentId) => {
+        try {
+            commentsAPI.remove(commentId);
+            console.log('useComments')
+
+        } catch (err) {
+            console.log(err.message)
+        }
+    }
+
+    return commentDeleteHandler;
+}
