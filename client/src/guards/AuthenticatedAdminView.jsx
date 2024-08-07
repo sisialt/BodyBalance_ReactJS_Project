abc8b01f79e9d285dbd1c0cc2391288c1f82da53
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-08-07 15:12:31 +0300

    Create AuthenticatedAdminView guard, only admin can create, edit, delete workouts

---
+++ client/src/guards/AuthenticatedAdminView.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { Navigate } from "react-router-dom";
+
+export default function AuthenticatedAdminView({children}) {
+    const { isAuthenticated, userId } = useContext(AuthContext);
+    const adminId = "60f0cf0b-34b0-4abd-9769-8c42f830dffc";
+
+    if (!isAuthenticated) {
+        return <Navigate to="/login" />;
+    };
+
+    if (userId !== adminId) {
+        return <Navigate to="/access-denied" />
+    }
+
+    return (
+        <>
+            {children}
+        </>
+    );
+}
