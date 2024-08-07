e2a2b8907039488bef2f555b26fe61e016d1c0cb
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-08-07 22:40:06 +0300

    Update auth-api, add username when register

--- client/src/api/auth-api.js
+++ client/src/api/auth-api.js
@@ -4,6 +4,6 @@ const BASE_URL = 'http://localhost:3030/users';
 
 export const login = (email, password) => requester.post(`${BASE_URL}/login`, { email, password });
 
-export const register = (email, password) => requester.post(`${BASE_URL}/register`, { email, password });
+export const register = (email, password) => requester.post(`${BASE_URL}/register`, { email, password, username: email.substring(0, email.indexOf("@")) });
 
 export const logout = () => requester.get(`${BASE_URL}/logout`);
