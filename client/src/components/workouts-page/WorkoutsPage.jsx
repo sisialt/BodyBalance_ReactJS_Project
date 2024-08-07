fd812647ca4f2003ca037b839f4d64edd6ae270b
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-08-07 15:15:00 +0300

    Update WorkoutsPage, only admin can see the add workout button

--- client/src/components/workouts-page/WorkoutsPage.jsx
+++ client/src/components/workouts-page/WorkoutsPage.jsx
@@ -3,14 +3,28 @@ import { Link } from 'react-router-dom';
 import './WorkoutsPage.css';
 
 import { useGetAllWorkouts } from '../../hooks/useWorkouts';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
 
 export default function WorkoutsPage() {
     const [workouts] = useGetAllWorkouts();
+    const { userId } = useContext(AuthContext);
+    const adminId = "60f0cf0b-34b0-4abd-9769-8c42f830dffc";
+    const isAdmin = userId === adminId;
 
     return (
         <>
             <div >
                 <h1 className="workouts-heading">Workouts</h1>
+
+                {isAdmin &&
+                    <div style={{ textAlign: 'center', marginTop: 50 }}>
+                        <Link to='create'>
+                            <input type="submit" className="submit" value="+ Add New Workout" />
+                        </Link>
+                    </div>
+                }
+
                 <div className="container workouts-page">
 
                     {workouts.map(workout =>
