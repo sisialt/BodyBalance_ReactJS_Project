6e4bafc69c34f77abbad84db8a384e75f85440e4
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-08-07 15:06:49 +0300

    Update AuthContext with pesisted state

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import usePersistedState from "../hooks/usePersistedState";
 
 export const AuthContext = createContext({
     userId: '',
@@ -10,7 +11,7 @@ export const AuthContext = createContext({
 });
 
 export function AuthContextProvider(props) {
-    const [authState, setAuthState] = useState({});
+    const [authState, setAuthState] = usePersistedState('auth', {});
 
     const changeAuthState = (state) => {
         setAuthState(state);
